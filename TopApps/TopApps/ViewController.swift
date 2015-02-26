5bed8c89cfad908afe53e1e222568353141d6846
Tim Fuqua <timfuqua@gmail.com>
2015-02-26 10:18:27 -0800

    JSON Parsing the standard Swift way. Quite tedious and ugly.

--- TopApps/TopApps/ViewController.swift
+++ TopApps/TopApps/ViewController.swift
@@ -12,7 +12,29 @@ import UIKit
 class ViewController: UIViewController {
   
   override func viewDidLoad() {
-    super.viewDidLoad()    
+    super.viewDidLoad()
+    
+    DataManager.getTopAppsDataFromFileWithSuccess { (data) -> Void in
+      
+      var parseError: NSError?
+      let parsedObject: AnyObject? = NSJSONSerialization.JSONObjectWithData(data,
+        options: NSJSONReadingOptions.AllowFragments,
+        error: &parseError)
+      
+      if let topApps = parsedObject as? NSDictionary {
+        if let feed = topApps["feed"] as? NSDictionary {
+          if let apps = feed["entry"] as? NSArray {
+            if let firstApp = apps[0] as? NSDictionary {
+              if let imname = firstApp["im:name"] as? NSDictionary {
+                if let appName = imname["label"] as? NSString {
+                  println("Optional Binding: \(appName)")
+                }
+              }
+            }
+          }
+        }
+      }
+    }
   }
 }
 
